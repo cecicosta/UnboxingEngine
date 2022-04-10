00e94ea0f162e443555565cc33e66db174e89f83
Cecilia Costa <cecier.costa@gmail.com>
2022-04-10 14:52:12 +0200

    vector3D code refactoring and change coordinates storage to array keeping the x, y, z abstraction.

--- include/vector3D.h
+++ include/vector3D.h
@@ -1,30 +1,34 @@
 #ifndef VECT3D_H
 #define VECT3D_H
 
-class vector3D
-{
-    public:
-    float x;
-    float y;
-    float z;
+#include <vector>
+class vector3D {
+
+    std::vector<float> values;
+public:
+    float& x;
+    float& y;
+    float& z;
 
     vector3D();
 
-    vector3D( float x, float y, float z );
+    vector3D(float x, float y, float z);
 
-    explicit vector3D( float vet[] );
+    explicit vector3D(const float array_values[]);
 
-    vector3D( const vector3D &v );
+    vector3D(const vector3D &v);
 
-    friend vector3D operator+ ( vector3D v1, vector3D v2 );
+    friend vector3D operator+(vector3D v1, vector3D v2);
 
-    friend vector3D operator- ( vector3D v1, vector3D v2 );
+    friend vector3D operator-(vector3D v1, vector3D v2);
 
-    friend vector3D operator* ( vector3D v, float escalar );
+    friend vector3D operator*(vector3D v, float escalar);
 
-    friend vector3D operator* ( float escalar, vector3D v );
+    friend vector3D operator*(float escalar, vector3D v);
 
-    friend vector3D operator/ ( vector3D v, float escalar );
+    friend vector3D operator/(vector3D v, float escalar);
+
+    vector3D& operator=(const vector3D& v);
 
     float modulo();
 
@@ -32,8 +36,9 @@ class vector3D
 
     float escalar(vector3D vet);
 
-    vector3D vetorial( vector3D B );
+    vector3D vetorial(vector3D B);
 
+    const float *ToArray() const;
 };
 
-#endif //VECT3D_H
+#endif//VECT3D_H
