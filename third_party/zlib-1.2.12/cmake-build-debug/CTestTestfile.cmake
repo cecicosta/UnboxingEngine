5502f384aff04a83fd924ea0d93a9d2eea80e8a5
Cecilia Costa <cecier.costa@gmail.com>
2022-04-30 11:15:14 +0200

    Adding project dependencies

---
+++ third_party/zlib-1.2.12/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Cecilia Costa/git/CatBoxEngine/third_party/zlib-1.2.12
+# Build directory: C:/Users/Cecilia Costa/git/CatBoxEngine/third_party/zlib-1.2.12/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
+set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Cecilia Costa/git/CatBoxEngine/third_party/zlib-1.2.12/CMakeLists.txt;235;add_test;C:/Users/Cecilia Costa/git/CatBoxEngine/third_party/zlib-1.2.12/CMakeLists.txt;0;")
+add_test(example64 "example64")
+set_tests_properties(example64 PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Cecilia Costa/git/CatBoxEngine/third_party/zlib-1.2.12/CMakeLists.txt;244;add_test;C:/Users/Cecilia Costa/git/CatBoxEngine/third_party/zlib-1.2.12/CMakeLists.txt;0;")
