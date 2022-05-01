cedc9f51bcca1782e586636b2db069170a858337
Cecilia Costa <cecier.costa@gmail.com>
2022-05-01 19:31:12 +0200

    Implement new tests for EventSystem and cleanup main.cpp

--- main.cpp
+++ main.cpp
@@ -2,40 +2,10 @@
 
 #include <MeshPrimitivesUtils.h>
 
-#include "CoreEvents.h"
-#include "Composite.h"
-
 using namespace unboxing_engine;
 
-
-class CEventListener: public core_events::ICoreEventsListener {
-public:
-    void OnStart() override {
-        std::cout << "OnStart called" << std::endl;
-    }
-    void OnUpdate() override {
-        std::cout << "OnUpdate called" << std::endl;
-    }
-    void OnInput() override {
-        std::cout << "OnInput called" << std::endl;
-    }
-    void OnPreRender() override {
-        std::cout << "OnPreRender called" << std::endl;
-    }
-    void OnPostRender() override {
-        std::cout << "OnPostRender called" << std::endl;
-    }
-    void OnRelease() override {
-        std::cout << "OnRelease called" << std::endl;
-    }
-};
-
 int main(int argc, char *argv[]) {
-
     CCore engine(640, 480, 32);
-
-    CEventListener eventListener;
-    engine.RegisterEventListener(eventListener);
     engine.Start();
 
     auto cube = primitive_utils::Cube();
--- tests/EventSystemTests.cpp
+++ tests/EventSystemTests.cpp
@@ -1,10 +1,13 @@
-#include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
 
 #include <CoreEvents.h>
 #include <EventDispatcher.h>
 
 using namespace unboxing_engine;
+
+using testing::Invoke;
+using testing::Return;
 
 class CEventListenerMock : public core_events::ICoreEventsListener {
 public:
@@ -18,7 +21,23 @@ public:
     MOCK_METHOD(void, OnRelease, (), (override));
 };
 
-class CEventDispatcherTest: public CEventDispatcher {
+class ITestListener {
+public:
+    virtual ~ITestListener() = default;
+    virtual void OnTest(int v) = 0;
+};
+
+class CEventListenerMockOther : public UListener<ITestListener, core_events::IUpdateListener, core_events::IPostRenderListener, core_events::IReleaseListener> {
+public:
+    ~CEventListenerMockOther() override = default;
+
+    MOCK_METHOD(void, OnTest, (int v), (override));
+    MOCK_METHOD(void, OnUpdate, (), (override));
+    MOCK_METHOD(void, OnPostRender, (), (override));
+    MOCK_METHOD(void, OnRelease, (), (override));
+};
+
+class CEventDispatcherTest : public CEventDispatcher {
 public:
     ~CEventDispatcherTest() override = default;
 };
@@ -30,10 +49,58 @@ public:
 protected:
     CEventDispatcherTest eventDispatcher;
     CEventListenerMock eventListenerMock;
+    CEventListenerMockOther eventListenerMockOther;
 };
 
-TEST_F(EventSystemFixture, register_and_recover_listener) {
 
-
-    ASSERT_EQ(1,1);
+TEST_F(EventSystemFixture, register_and_recover_listener) {//NOLINT (gtest static memory warning for type_info_
+    eventDispatcher.RegisterListener(eventListenerMock);
+    EXPECT_CALL(eventListenerMock, OnStart).Times(1);
+    for (auto &&listener: eventDispatcher.GetListeners<core_events::IStartListener>()) {
+        listener->OnStart();
+    }
 }
+
+TEST_F(EventSystemFixture, register_and_recovery_different_type_listeners_for_object) {//NOLINT (gtest static memory warning for type_info_)
+    eventDispatcher.RegisterListener(eventListenerMock);
+    EXPECT_CALL(eventListenerMock, OnStart).Times(1);
+    EXPECT_CALL(eventListenerMock, OnUpdate).Times(1);
+    EXPECT_CALL(eventListenerMock, OnInput).Times(1);
+    for (auto &&listener: eventDispatcher.GetListeners<core_events::IStartListener>()) {
+        listener->OnStart();
+    }
+    for (auto &&listener: eventDispatcher.GetListeners<core_events::IUpdateListener>()) {
+        listener->OnUpdate();
+    }
+    for (auto &&listener: eventDispatcher.GetListeners<core_events::IInputListener>()) {
+        listener->OnInput();
+    }
+}
+
+TEST_F(EventSystemFixture, register_multiple_objects_and_recover_each_specific_type_listener) {//NOLINT (gtest static memory warning for type_info_)
+    int test_value = 10;
+    int test_result = 0;
+    eventDispatcher.RegisterListener(eventListenerMock);
+    eventDispatcher.RegisterListener(eventListenerMockOther);
+
+    auto updateListeners = eventDispatcher.GetListeners<core_events::IUpdateListener>();
+    EXPECT_EQ(updateListeners.size(), 2);
+
+    auto testListeners = eventDispatcher.GetListeners<ITestListener>();
+    EXPECT_EQ(testListeners.size(), 1);
+
+    EXPECT_CALL(eventListenerMock, OnUpdate).Times(1);
+    EXPECT_CALL(eventListenerMockOther, OnUpdate).Times(1);
+    EXPECT_CALL(eventListenerMockOther, OnTest).Times(1);
+    ON_CALL(eventListenerMockOther, OnTest(test_value)).WillByDefault(Invoke([&test_result](int v) { test_result = v; }));
+
+    for (auto &&l: updateListeners) {
+        l->OnUpdate();
+    }
+    for (auto &&l: testListeners) {
+        l->OnTest(test_value);
+    }
+
+    ASSERT_EQ(test_value, test_result);
+}
+
