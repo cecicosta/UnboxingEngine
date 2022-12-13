815afe2e7ea379adbe8fe821f10ff0c907c6bb76
Cecilia Costa <cecier.costa@gmail.com>
2022-12-13 13:06:04 +0100

    Turning algorithms into templates so to be used on 3D collisions as well as 2D

--- tests/AlgorithmsTests.cpp
+++ tests/AlgorithmsTests.cpp
@@ -4,14 +4,16 @@
 #include <UVector.h>
 
 #include <algorithm>
+#include <assert.h>
 
 namespace MyCollision {
 
+template <typename T, int dimension>
 struct SCollisionResult {
-    std::vector<Vector2f> vertices; // Vertices representing the edge(s) or shape hit
-    Vector2f normal; // Resulting normal on the hit point
-    Vector2f intersection; // Intersection point
-    Vector2f escape; //Vector to which direction and lenth the collision can be undone
+    std::vector<Vector<T, dimension>> vertices;// Vertices representing the edge(s) or shape hit
+    Vector<T, dimension> normal;// Resulting normal on the hit point
+    Vector<T, dimension> intersection;   // Intersection point
+    Vector<T, dimension> escape;      //Vector to which direction and lenth the collision can be undone
 };
 
 /// <summary>
@@ -23,16 +25,20 @@ struct SCollisionResult {
 /// <param name="p2"></param>
 /// <param name="projectionLimits"></param>
 /// <returns></returns>
-bool hasProjectionOverEdge(const Vector2f &start, const Vector2f &end, const Vector2f &p1, const Vector2f &p2, std::pair<float, float>& projectionLimits) {
-    Vector2f axis = end - start;
-    float axisLength = axis.Length();
-    axis = axis.Normalized();
-    float p1ProjectionOverAxis = (p1-start).DotProduct(axis);
-    float p2ProjectionOverAxis = (p2-start).DotProduct(axis);
-    float max = std::max(p1ProjectionOverAxis, p2ProjectionOverAxis);
-    float min = std::min(p1ProjectionOverAxis, p2ProjectionOverAxis);
-    projectionLimits = std::pair(min, max);
-    return max > 0 && min < axisLength;
+template <typename T, int dimention>
+bool findProjectedPointsOverSegment(const Vector<T, dimention> &start, const Vector<T, dimention> &end, const std::vector<Vector<T, dimention>> &points, std::vector<T> &projected_points) {
+    if (points.size() == 0) {
+        return false;
+    }
+    auto segment_direction = end - start;
+    auto segment_length = segment_direction.Length();
+    segment_direction = segment_direction.Normalized();
+    
+    for (const auto& p : points) {
+        projected_points.emplace_back((p - start).DotProduct(segment_direction));
+    }
+    std::sort(projected_points.begin(), projected_points.end());
+    return *(projected_points.end() - 1) > 0 && *projected_points.begin() < segment_length;
 }
 
 /// <summary>
@@ -43,8 +49,9 @@ bool hasProjectionOverEdge(const Vector2f &start, const Vector2f &end, const Vec
 /// <param name="l2_point"></param>
 /// <param name="l2_dir"></param>
 /// <returns></returns>
-Vector2f findIntersectionBetweenLines(const Vector2f &l1_point, const Vector2f& l1_dir, const Vector2f &l2_point, const Vector2f & l2_dir) {
-    auto l1_ortogonal_axis = Vector2f(l1_dir.y, -l1_dir.x);//Rotate the edge clockwise to obtain the normal
+template <typename T, int dimension>
+Vector<T, dimension> findIntersectionBetweenLines(const Vector<T, dimension> &l1_point, const Vector<T, dimension> &l1_dir, const Vector<T, dimension> &l2_point, const Vector<T, dimension> &l2_dir) {
+    auto l1_ortogonal_axis = Vector<T, dimension>(l1_dir.y, -l1_dir.x);//Rotate the edge clockwise to obtain the normal
     auto distance_l2_to_l1 = l1_point - l2_point;
     auto distance_ortogonal_component = (distance_l2_to_l1).DotProduct(l1_ortogonal_axis);
     auto direction_ortogonal_component = l2_dir.DotProduct(l1_ortogonal_axis);
@@ -52,15 +59,16 @@ Vector2f findIntersectionBetweenLines(const Vector2f &l1_point, const Vector2f& 
     return l2_point + l2_dir * dt;
 }
 
-Vector2f getResultingNormalFromVertices(const std::vector<Vector2f>& vertices) {
+template <typename T, int dimension>
+Vector<T, dimension> getResultingNormalFromVertices(const std::vector<Vector<T, dimension>> &vertices) {
     if (vertices.size() < 2) {
         return {};
     }
-    Vector2f normal;
+    Vector<T, dimension> normal;
     auto v = vertices.begin();
     for (; v != vertices.end() - 1; v++) {
         auto direction = *(v + 1) - *v;
-        normal = normal + Vector2f(direction.y, -direction.x).Normalized();
+        normal = normal + Vector<T, dimension>(direction.y, -direction.x).Normalized();
     }
     return normal.Normalized();
 }
@@ -74,13 +82,14 @@ Vector2f getResultingNormalFromVertices(const std::vector<Vector2f>& vertices) {
 /// <param name="connectLastVertex"> Define if the last vertex of the list should be considered connected to the first. The default values is true. </param>
 /// <returns>Returns a SCollisionResult structure with the information of vertices belonging to the edges hit, resulting normal and point of intersection.
 /// The only case more than one edge is considered to be hit is if the intersection occurs at the point of intersections between the two edges.</returns>
-SCollisionResult findEdgesHitOnTrajectory(const std::vector<Vector2f>& vertices, const Vector2f& start, const Vector2f& end, bool connectLastVertex = true) {
+template <typename T, int dimension>
+SCollisionResult<T, dimension> findEdgesHitOnTrajectory(const std::vector<Vector<T, dimension>> &vertices, const Vector<T, dimension> &start, const Vector<T, dimension> &end, bool connectLastVertex = true) {
     if (vertices.size() < 2) {
         return {};
     }
 
-    Vector2f previous;
-    std::vector<Vector2f>::const_iterator v;
+    Vector<T, dimension> previous;
+    std::vector<Vector<T, dimension>>::const_iterator v;
     if (connectLastVertex) {
         previous = *(vertices.end() - 1);
         v = vertices.begin();
@@ -95,7 +104,7 @@ SCollisionResult findEdgesHitOnTrajectory(const std::vector<Vector2f>& vertices,
         auto edge_direction = edge.Normalized();
         auto edge_origin = previous; //v1
         auto normal = edge_direction.Normalized();
-        normal = Vector2f(edge_direction.y, -edge_direction.x);//Rotate the edge clockwise to obtain the normal
+        normal = Vector<T, dimension>(edge_direction.y, -edge_direction.x);//Rotate the edge clockwise to obtain the normal
         auto direction = (end - start).Normalized();
         //The potentially colliding edge must have a normal with projection to the oposite side of the direction
         if ((normal).DotProduct(direction) > 0) {
@@ -116,7 +125,7 @@ SCollisionResult findEdgesHitOnTrajectory(const std::vector<Vector2f>& vertices,
             //Such scenario can be found by only checking the if second condition. If the hit is at the edge's length, the next edge was hit as well. No further check is needed.
             //Interpolate the normal of both edges.
             if (projection_intersection_point_to_edge_length == edge_length && (v + 1 !=  vertices.end())) {
-                return {{previous, *v, *(v + 1)}, getResultingNormalFromVertices({previous, *v, *(v + 1)}), extrapolate_trajectory_and_edge_intersection};
+                return {{previous, *v, *(v + 1)}, getResultingNormalFromVertices<T, dimension>({previous, *v, *(v + 1)}), extrapolate_trajectory_and_edge_intersection};
             }
             return {{previous, *v}, normal, extrapolate_trajectory_and_edge_intersection};
         }
@@ -133,33 +142,32 @@ SCollisionResult findEdgesHitOnTrajectory(const std::vector<Vector2f>& vertices,
 /// <param name="d"></param>
 /// <param name="direction"></param>
 /// <returns></returns>
-static SCollisionResult checkSegmentCollisionAgainstBox(const std::vector<Vector2f> &box, const Vector2f &c, const Vector2f &d, const Vector2f& direction) {
-    Vector2f v1 = box[0];
-    Vector2f v2 = box[1];
-    Vector2f v3 = box[2];
-    Vector2f v4 = box[3];
-    
-    auto GetEscapeVectorOposeToMovement = [&](std::pair<float, float> projected_vertices, const Vector2f& v1, const Vector2f& v2) {
-        Vector2f edgeDir = (v2 - v1).Normalized();
-        Vector2f escape1 = - projected_vertices.second*edgeDir;
-        Vector2f escape2 = (v2 - v1) - projected_vertices.first*edgeDir;
-        return (escape1).DotProduct(direction) < 0 ? escape1 : escape2;
+template <typename T, int dimension>
+static SCollisionResult<T, dimension> checkSegmentCollisionAgainstRect(const std::vector<Vector<T, dimension>> &box, const Vector<T, dimension> &c, const Vector<T, dimension> &d, const Vector<T, dimension> &direction) {   
+    auto find_escape_vector_component_oposing_direction = [&direction](std::pair<float, float> projected_vertices, const Vector<T, dimension> &edge) {
+        auto edge_direction = edge.Normalized();
+        auto escape_towards = - projected_vertices.second*edge_direction;
+        auto escape_backwards = edge - projected_vertices.first*edge_direction;
+        return (escape_towards).DotProduct(direction) < 0 ? escape_towards : escape_backwards;
     };
     
     bool hasIntersection = false;
-    std::pair<float, float> projectionValues;
+    std::vector<float> projected_points;
     
-    if(hasProjectionOverEdge(v1, v2, c, d, projectionValues)) {
-        auto escapeV1 = GetEscapeVectorOposeToMovement(projectionValues, v1, v2);
-        
-        if (hasProjectionOverEdge(v2, v3, c, d, projectionValues)) {
-            auto escapeV2 = GetEscapeVectorOposeToMovement(projectionValues, v2, v3);
-            float escape_v1_project_dir = (escapeV1).DotProduct(direction);
-            float escape_v2_project_dir = (escapeV2).DotProduct(direction);
+    if (findProjectedPointsOverSegment(box[0], box[1], {c, d}, projected_points)) {
+        assert(projected_points.size() == 2 && "Number of projected points do not match expectation.");
+        auto escape_component_v1_v2 = find_escape_vector_component_oposing_direction({projected_points[0], projected_points[1]}, box[1] - box[0]);
+
+        projected_points.clear();
+        if (findProjectedPointsOverSegment(box[1], box[2], {c, d}, projected_points)) {
+            assert(projected_points.size() == 2 && "Number of projected points do not match expectation.");
+            auto escape_component_v2_v3 = find_escape_vector_component_oposing_direction({projected_points[0], projected_points[1]}, box[2] - box[1]);
+            T escape_v1_project_dir = (escape_component_v1_v2).DotProduct(direction);
+            T escape_v2_project_dir = (escape_component_v2_v3).DotProduct(direction);
 
             auto escape = std::abs(escape_v1_project_dir) < std::abs(escape_v2_project_dir) ? 
-               findIntersectionBetweenLines({0, 0}, direction, escapeV1, Vector2f(escapeV1.y, -escapeV1.x).Normalized()) : 
-                findIntersectionBetweenLines({0, 0}, direction, escapeV2, Vector2f(escapeV2.y, -escapeV2.x).Normalized());
+               findIntersectionBetweenLines({0, 0}, direction, escape_component_v1_v2, Vector<T, dimension>(escape_component_v1_v2.y, -escape_component_v1_v2.x, escape_component_v1_v2.z).Normalized()) : 
+                findIntersectionBetweenLines({0, 0}, direction, escape_component_v2_v3, Vector<T, dimension>(escape_component_v2_v3.y, -escape_component_v2_v3.x, escape_component_v2_v3.z).Normalized());
             auto result = findEdgesHitOnTrajectory({box[0], box[1], box[2], box[3]}, c, d);
             result.escape = escape;
 
@@ -176,24 +184,28 @@ static SCollisionResult checkSegmentCollisionAgainstBox(const std::vector<Vector
 /// <param name="box2"></param>
 /// <param name="direction"></param>
 /// <returns></returns>
-bool CheckBoxToBoxCollision(std::vector<Vector2f> box1, std::vector<Vector2f> box2, const Vector2f& direction) {
+template <typename T, int dimension>
+bool CheckBoxToBoxCollision(std::vector<Vector<T, dimension>> box1, std::vector<Vector<T, dimension>> box2, const Vector<T, dimension> &direction, SCollisionResult<T, dimension> &result) {
     if(box1.size() < 4 && box2.size() < 4) {
         return false;
     }
     
     //Algorithm for checking if there is an intersection between the bounding boxes by projecting one's edges against any 2 orthogonal edges of the other.
     //If any of the first's edges has such projection intersecting both orthogonal faces, the bounding boxes intersect each other
-    bool hasIntersection = false;
     for (int v = 1; v < 4; v++) {
-        Vector2f c = box2[v-1];
-        Vector2f d = box2[v];
+        auto c = box2[v-1];
+        auto d = box2[v];
         
         //Since the bounding box is a rectangulum, calculating the projection to one triangle, by symmetry, the other triangle will also contain the projection
-        Vector2f escape;
-        Vector2f normal;
-        hasIntersection = findEdgesHitOnTrajectory(box1, c, d).vertices.size() > 0;
+        auto escape;
+        auto normal;
+        result = findEdgesHitOnTrajectory(box1, c, d);
+
+        if (result.vertices.size() > 0) {
+            return true;
+        }
     }
-    return hasIntersection;
+    return false;
 }
 
 static std::vector<Vector2f> GetBoxVerticesFromCircleTrajectory(const Vector2f& p1, const Vector2f& p2, float radius, const Vector2f& direction) {
@@ -209,7 +221,6 @@ static std::vector<Vector2f> GetBoxVerticesFromCircleTrajectory(const Vector2f& 
 
 }
 
-
 bool AssessIntersectionBoxWithRay(const BoundingBox &boundingBox, const Vector3f &raySrc, const Vector3f &rayDir) {
     //Derivative from p >= min, p >= max, p within [min, max], the 2 points defining the box. And p = O + Dx/Dt, from the ray, with p representing any point of intersection
     Vector3f upper = (boundingBox.getFirst() + boundingBox.getSecond()) - 2 * raySrc;
@@ -224,7 +235,7 @@ TEST(GeneralCollisionHelpers, TraceTrajectoryIntoBox_) {
     Vector2f start(-11, 6);
     Vector2f end(-6, 8);
     Vector2f direction = Vector2f(1.5f, 0.5f).Normalized();
-    auto result = MyCollision::checkSegmentCollisionAgainstBox(box, start, end, direction);
+    auto result = MyCollision::checkSegmentCollisionAgainstRect(box, start, end, direction);
     if (result.vertices.size() == 2) {
         ASSERT_EQ(result.vertices[0], box[3]);
         ASSERT_EQ(result.vertices[1], box[0]);
@@ -241,7 +252,7 @@ TEST(GeneralCollisionHelpers, TraceTrajectoryIntoBox_ValidateIntersectionResults
 
     Vector2f start(-11, 6);
     Vector2f end(-6, 8);
-    auto result = MyCollision::findEdgesHitOnTrajectory(box, start, end);
+    auto result = MyCollision::findEdgesHitOnTrajectory<float, 2>(box, start, end);
     EXPECT_EQ(result.vertices.size(), 2);
     if (result.vertices.size() == 2) {
         ASSERT_EQ(result.vertices[0], box[3]);
@@ -257,8 +268,8 @@ TEST(GeneralCollisionHelpers, HasIntersectionOverEdge_TRUE) {
     Vector2f p2(7, 8);
     Vector2f start(4, 3);
     Vector2f end(10, 4);
-    std::pair<float, float> value;
-    ASSERT_TRUE(MyCollision::hasProjectionOverEdge(start, end, p1, p2, value));
+    std::vector<float> value;
+    ASSERT_TRUE(MyCollision::findProjectedPointsOverSegment(start, end, {p1, p2}, value));
 
 }
 
@@ -267,8 +278,8 @@ TEST(GeneralCollisionHelpers, HasIntersectionOverEdge_False) {
     Vector2f p2(-9.5, 4.2);
     Vector2f start(-10, 4.5);
     Vector2f end(-8, 8);
-    std::pair<float, float> value;
-    ASSERT_FALSE(MyCollision::hasProjectionOverEdge(start, end, p1, p2, value));
+    std::vector<float> value;
+    ASSERT_FALSE(MyCollision::findProjectedPointsOverSegment(start, end, {p1, p2}, value));
 }
 
 TEST(CubeRayIntersectionTest, DetectIntersection) {
@@ -311,30 +322,18 @@ TEST(CubeRayIntersectionTest, TangentIntersection) {
     ASSERT_TRUE(AssessIntersectionBoxWithRay(BoundingBox(first, second), raySrc, rayEnd - raySrc));
 }
 
-//Algorithm for checking if an edge ab, has an intersecting projection over the other, cd
-bool hasProjectingIntersection(const Vector3f &a, const Vector3f &b, const Vector3f &c, const Vector3f &d) {
-    Vector3f edge = b - a;
-    float ab = edge.Length();
-    edge = edge.Normalized();
-    float e = (c - a).DotProduct(edge);
-    float f = (d - a).DotProduct(edge);
-    float max = std::max(e, f);
-    float min = std::min(e, f);
-    return max >= 0 && min <= ab;
-}
-
-TEST(EdgeProjectingIntersection, EdgeProjectionHasIntersection) {
-    Vector3f a(0, 0, 0);
-    Vector3f b(10, 0, 0);
-    Vector3f c(8, 5, 0);
-    Vector3f d(15, 10, 0);
-    ASSERT_TRUE(hasProjectingIntersection(a, b, c, d));
-}
-
 //The algorithms is designed so to consider the boxes with its transformations applied
 TEST(CubeRayIntersectionTest, FaceBoxIntersection) {
     BoundingBox box1(Vector3f(0, 0, 0), Vector3f(5, 7, 5));
     BoundingBox box2(Vector3f(0, 0, 4.9), Vector3f(8, 10, 9));
+    Vector3f direction;
+
+    auto find_escape_vector_component_oposing_direction = [&direction](std::pair<float, float> projected_vertices, const Vector3f &edge) {
+        auto edge_direction = edge.Normalized();
+        auto escape_towards = -projected_vertices.second * edge_direction;
+        auto escape_backwards = edge - projected_vertices.first * edge_direction;
+        return (escape_towards).DotProduct(direction) < 0 ? escape_towards : escape_backwards;
+    };
 
     //Algorithm for checking if there is an intersection between the bounding boxes by projecting one's edges points against any 2 orthogonal faces of the other.
     //If any of the first's edge's points has such projection intersecting both orthogonal faces, the bounding boxes intersect each other
@@ -345,11 +344,14 @@ TEST(CubeRayIntersectionTest, FaceBoxIntersection) {
             Vector3f d = box1.GetVertex(static_cast<EFace>(f), v);
 
             //Since the bounding box is a cuboid, calculating the projection to one triangle, by symmetry, the other triangle will also contain the projection
-            auto checkTriangleIntersection = [&c, &d, &box2](EFace face) {
+            auto checkTriangleIntersection = [&c, &d, &box2, &direction](EFace face) {
                 Vector3f v1 = box2.GetVertex(face, 0);
                 Vector3f v2 = box2.GetVertex(face, 1);
                 Vector3f v3 = box2.GetVertex(face, 2);
-                return hasProjectingIntersection(v1, v2, c, d) && hasProjectingIntersection(v2, v3, c, d);
+                Vector3f v4 = box2.GetVertex(face, 3);
+                std::vector<float> projections;
+                MyCollision::checkSegmentCollisionAgainstRect({v1, v2, v3, v4}, c, d, direction);
+                return false; 
             };
             if (checkTriangleIntersection(EFace::BOTTOM) && checkTriangleIntersection(EFace::BACK)) {
                 hasIntersection = true;
